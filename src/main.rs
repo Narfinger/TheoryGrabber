b15b5bb0d72f8e2257c8776df5a8e1954ab2f2f4
Narfinger <Narfinger@users.noreply.github.com>
2017-10-23 17:42:23 +0530

    some stubs done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate rss;
+extern crate url;
+
+use rss::Channel;
+use url::Url;
+
+static ARXIV: &'static str = "http://arxiv.org/rss/cs.CC";
+static ECCC: &'static str = "http://eccc.hpi-web.de/feeds/reports/";
+
+struct Paper {
+    title: String,
+    description: String,
+    link: url::Url,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let channel = Channel::from_url(ARXIV).unwrap();
+
+    let papers = channel
+        .items()
+        .into_iter()
+        .map(|i| {
+            let title = i.title().unwrap();
+            let description = i.description().unwrap();
+            let link = i.link().unwrap();
+
+            Paper {
+                title: title,
+                description: description,
+                link: Url::parse(link),
+            }
+        });
+
 }
