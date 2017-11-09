29b8fa011451353c53f4954c194e1d553f46e1cc
Narfinger <Narfinger@users.noreply.github.com>
2017-11-09 13:44:03 +0530

    more stubs

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ extern crate rss;
 extern crate serde_yaml;
 #[macro_use]
 extern crate serde_derive;
+extern crate tempdir;
 extern crate url;
 extern crate quick_xml;
 
@@ -33,9 +34,8 @@ use cursive::views::{Dialog, LinearLayout, TextView, DummyView};
 use cursive_table_view::{TableView, TableViewItem};
 use cursive::traits::*;
 use indicatif::ProgressBar;
-use std::{thread, time};
-use std::rc::Rc;
 use types::{Paper, print_authors};
+use tempdir::TempDir;
 use arxiv::parse_arxiv;
 
 static ECCC: &'static str = "http://eccc.hpi-web.de/feeds/reports/";
@@ -83,70 +83,72 @@ impl TableViewItem<BasicColumn> for Paper {
 
 
 fn download_papers(papers: &[Paper]) {
-    println!("{:?}", papers);
-    println!("DOING STUFF");
-    let ten_millis = time::Duration::from_millis(1000);
-    //this quits now
-    thread::sleep(ten_millis);
+    let progressbar = ProgressBar::new(papers.len() as u64);
+    if let Ok(dir) = TempDir::new("TheoryGrabber") {
+        for i in papers {
+            progressbar.inc(1);
+        }
+    }
+    progressbar.finish();
 }
 
-fn build_gui(papers: Vec<Paper>, download: Rc<bool>) -> Vec<Paper> {
-    let mut quit = false;
-    let mut p: Vec<Paper> = Vec::new();
-    {
-        let mut siv = Cursive::new();
-        let mut table = TableView::<Paper, BasicColumn>::new()
-            .column(BasicColumn::Title,
-                    "Title",
-                    |c| c.ordering(std::cmp::Ordering::Greater).width_percent(75))
-            .column(BasicColumn::Source,
-                    "Source",
-                    |c| c.ordering(std::cmp::Ordering::Greater))
-            .column(BasicColumn::Published,
-                    "Published",
-                    |c| c.ordering(std::cmp::Ordering::Greater));
+fn build_gui(papers: Vec<Paper>) -> Vec<Paper> {
+    let mut siv = Cursive::new();
+    let mut table = TableView::<Paper, BasicColumn>::new()
+        .column(BasicColumn::Title,
+                "Title",
+                |c| c.ordering(std::cmp::Ordering::Greater).width_percent(75))
+        .column(BasicColumn::Source,
+                "Source",
+                |c| c.ordering(std::cmp::Ordering::Greater))
+        .column(BasicColumn::Published,
+                "Published",
+                |c| c.ordering(std::cmp::Ordering::Greater));
 
-        table.set_items(papers);
-        table.set_on_submit(|siv: &mut Cursive, row: usize, index: usize| {
+    table.set_items(papers);
+    table.set_on_submit(|siv: &mut Cursive, row: usize, index: usize| {
 
-            let value: Paper = siv.call_on_id("table",
-                                              move |table: &mut TableView<Paper, BasicColumn>| {
-                                                  table.borrow_item(index).unwrap().clone()
-                                              })
-                .unwrap();
+        let value: Paper = siv.call_on_id("table",
+                                          move |table: &mut TableView<Paper, BasicColumn>| {
+                                              table.borrow_item(index).unwrap().clone()
+                                          })
+            .unwrap();
 
-            let d = LinearLayout::vertical()
-                .child(TextView::new(value.title.clone()))
-                .child(DummyView)
-                .child(TextView::new(print_authors(&value)))
-                .child(DummyView)
-                .child(TextView::new(value.description.clone()));
+        let d = LinearLayout::vertical()
+            .child(TextView::new(value.title.clone()))
+            .child(DummyView)
+            .child(TextView::new(print_authors(&value)))
+            .child(DummyView)
+            .child(TextView::new(value.description.clone()));
 
-            siv.add_layer(Dialog::around(d)
-                              .title(format!("Removing row # {}", row))
-                              .button("Delete", move |s| {
-                s.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
-                    table.remove_item(index);
-                });
-                                  s.pop_layer()
-            })
-                              .button("Close", move |s| s.pop_layer()));
-        });
+        siv.add_layer(Dialog::around(d)
+                      .title(format!("Removing row # {}", row))
+                      .button("Delete", move |s| {
+                          s.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
+                              table.remove_item(index);
+                          });
+                          s.pop_layer()
+                      }).button("Close", move |s| s.pop_layer()));
+    });
 
-        siv.add_layer(Dialog::around(table.with_id("table").min_size((500, 80)))
-                          .title("Table View")
-                      .button("Download all", move |s| {
-                          *Rc::get_mut(&mut download).unwrap() = true;
+    siv.add_layer(Dialog::around(table.with_id("table").min_size((500, 80)))
+                  .title("Table View")
+                  .button("Download all", move |s| {
                       s.quit()})
-                          .button("Quit", |s| s.quit()));
+                  .button("Quit", move |s| {
+                      s.call_on_id("table", move |table: &mut TableView<Paper, BasicColumn>| {
+                          table.clear();
+                      });
+                      s.quit();
+                  })
+    );
 
-        //pb.finish_and_clear();
+    //pb.finish_and_clear();
 
-        siv.run();
-        siv.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
-            table.take_items()
-        }).unwrap()
-    }
+    siv.run();
+    siv.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
+        table.take_items()
+    }).unwrap()
 }
 
 fn main() {
@@ -156,7 +158,9 @@ fn main() {
     //currently disabled
     //config::write_now();
     
-    let mut download = Rc::new(false);
-    let filtered_papers = build_gui(filtered_papers, download);
-    println!("{:?}", filtered_papers);
+    let filtered_papers = build_gui(filtered_papers);
+    download_papers(&filtered_papers);
+    
+    
+    //    println!("{:?}", filtered_papers);
 }
