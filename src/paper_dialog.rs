244cb8832439869693ed26e6b89eeac01075cc7d
Narfinger <Narfinger@users.noreply.github.com>
2017-11-20 20:34:22 +0530

    a bit of refactoring

--- src/paper_dialog.rs
+++ src/paper_dialog.rs
@@ -22,11 +22,16 @@ impl View for PaperDialog {
     fn draw(&self, printer: &Printer)              { self.d.draw(printer); }
     fn required_size(&mut self, req: Vec2) -> Vec2 { self.d.required_size(req) }
     fn layout(&mut self, size: Vec2)               { self.d.layout(size); }
-    fn on_event(&mut self, event: Event) -> EventResult { self.d.on_event(event) }
+    fn on_event(&mut self, event: Event) -> EventResult { custom_event_handler(self, event) }
     fn take_focus(&mut self, source: Direction) -> bool { self.d.take_focus(source) }
     fn call_on_any<'a>(&mut self, selector: &Selector, callback: Box<FnMut(&mut Any) + 'a>) { self.d.call_on_any(selector, callback); }
     fn focus_view(&mut self, selector: &Selector) -> Result<(), ()> { self.d.focus_view(selector) }
     fn needs_relayout(&self) -> bool               { self.d.needs_relayout() }
+}
+
+fn custom_event_handler(pd: &mut PaperDialog, event:Event) -> EventResult {
+    //pd.call_on_id(button delete)
+    pd.d.on_event(event)
 }
 
 pub fn new(value: &Paper, row: usize, index: usize) -> PaperDialog {
@@ -38,26 +43,24 @@ pub fn new(value: &Paper, row: usize, index: usize) -> PaperDialog {
         .child(TextView::new(value.link.clone().to_string()))
         .child(DummyView)
         .child(TextView::new(value.description.clone()));
-
-
-    PaperDialog { d: 
-                  Dialog::around(nd)
-                  .title(format!("Details row # {}", row))
-                  .button("Next", move |s| {
-                      //this is kind of hacky
-                      s.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
-                          //this is technically not correct as index+1 is in the unsorted view, while we look in the sorted one
-                          table.set_selected_row(index+1);
-                      });
-                      s.pop_layer();
-                      //s.on_event(cursive::event::Event::Key(cursive::event::Key::Enter));
-                  })
-                  .button("Delete", move |s| {
-                      s.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
-                          table.remove_item(index);
-                      });
-                      s.pop_layer()
-                  })
-                  .button("Close", move |s| s.pop_layer())
-    }
+    let dialog = Dialog::around(nd)
+        .title(format!("Details row # {}", row))
+        .button("Next", move |s| {
+            //this is kind of hacky
+            s.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
+                //this is technically not correct as index+1 is in the unsorted view, while we look in the sorted one
+                table.set_selected_row(index+1);
+            });
+            s.pop_layer();
+            //s.on_event(cursive::event::Event::Key(cursive::event::Key::Enter));
+        })
+        .button("Delete", move |s| {
+            s.call_on_id("table", |table: &mut TableView<Paper, BasicColumn>| {
+                table.remove_item(index);
+            });
+            s.pop_layer()
+        })
+        .button("Close", move |s| s.pop_layer());
+        
+    PaperDialog { d: dialog }
 }
