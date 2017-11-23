f81c37d657a105e584ad23646803f12bbecda266
Narfinger <Narfinger@users.noreply.github.com>
2017-11-23 22:03:40 +0530

    we now extract the authors

--- src/eccc.rs
+++ src/eccc.rs
@@ -13,6 +13,16 @@ fn get_url() -> String {
     ECCC.to_string() + &Utc::now().year().to_string() + "/"
 }
 
+/// this extracts the authors in the following way
+/// we split at whitespace, skip the first 5 which are id and link
+/// we construct enumerates for all authors, to merge adjacent authors together, i.e., ["Foo" "Bar"] -> ["Foo Bar"]
+fn extract_authors(authors_raw: String) -> Vec<String> {
+    let enumerated_strings = authors_raw.split_whitespace().skip(5).map(String::from).enumerate();
+    let (even, odd): (Vec<(usize, String)>, Vec<(usize, String)>) = enumerated_strings.partition(|&(x,ref el)| x % 2 == 0);
+    let authors = even.into_iter().zip(odd).map(|((_,x),(_,y))| x + " " + &y).collect::<Vec<String>>();
+
+    authors
+}
 
 /// The full abstract is in the details so we better filter before we look into the details of a million papers. Hence this function needs a filter time
 pub fn parse_eccc(utc: DateTime<Utc>) -> Result<Vec<Paper>> {
@@ -20,7 +30,6 @@ pub fn parse_eccc(utc: DateTime<Utc>) -> Result<Vec<Paper>> {
     if !res.status().is_success() {
         return Err("Some error in getting the reqwuest".into());
     } 
-
     
     let mut res_string = String::new();
     res.read_to_string(&mut res_string)?;
@@ -36,19 +45,19 @@ pub fn parse_eccc(utc: DateTime<Utc>) -> Result<Vec<Paper>> {
         let id_and_date_raw = div.find(Name("u")).nth(0).unwrap().text();
         let link_raw = div.find(Name("a")).nth(0).unwrap().attr("href").unwrap();
         let title_raw = div.find(Name("h4")).nth(0).unwrap().text();
-        let abs_raw = div.find(And(Name("div"), Attr("style", "text-align:justify;"))).nth(0).unwrap().text();
-        let authors_raw = "";//div.text();
+//        let abs_raw = div.find(And(Name("div"), Attr("style", "text-align:justify;"))).nth(0).unwrap().text();
+        let authors_raw = div.children().nth(1).unwrap().text();
 
         let id_and_date = id_and_date_raw.trim();
         let link = link_raw.trim();
         let title = title_raw.trim();
-        let abs = abs_raw.trim();
-        let authors = authors_raw.trim();
+//        let abs = abs_raw.trim();
+        let authors = extract_authors(authors_raw);
 
 
         //we need to get the full abstract from the details page
-        
-        println!("Parsed: idd{:?}, link{:?}, title{:?}, abs{:?} authors{:?}", id_and_date, link, title, abs, authors);
+        //println!("authors: {}", authors);
+        println!("Parsed: idd{:?}, link{:?}, title{:?}, authors{:?}", id_and_date, link, title, authors);
     } 
     
     
