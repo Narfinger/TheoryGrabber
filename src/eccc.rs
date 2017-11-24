0a45467edc3aac70e818b9569589c5b4a761ae70
Narfinger <Narfinger@users.noreply.github.com>
2017-11-24 17:45:42 +0530

    some more slight changes

--- src/eccc.rs
+++ src/eccc.rs
@@ -8,9 +8,11 @@ use nom::digit;
 use nom::{IResult};
 use select::document::Document;
 use select::predicate::{Attr, And, Class, Element, Name, Predicate};
-use types::Paper;
+use select::node::Node;
+use types::{Source, Paper};
 
 static ECCC: &'static str = "https://eccc.weizmann.ac.il/year/";
+static BASE_URL: &'static str = "https://eccc.weizmann.ac.il";
 
 ///eccc has a custom format chrono cannot parse, so we build a parser with nom
 named!(number<u32>, map_res!(map_res!(digit,from_utf8), FromStr::from_str) );
@@ -61,7 +63,7 @@ fn date_parse_test() {
     assert_eq!(parse_rough_date (" 19th January 2017"), Some(NaiveDate::from_ymd(2017,1,19)));
 }
 
-
+/// The url we should look at. This is kind of rough as we just use the current year, hoping that nobody publishes around new year.
 fn get_url() -> String {
     ECCC.to_string() + &Utc::now().year().to_string() + "/"
 }
@@ -77,24 +79,44 @@ fn extract_authors(authors_raw: String) -> Vec<String> {
     authors
 }
 
-fn extract_id_rough_date(id_date_raw: String) -> (String, String) {
+fn extract_id_rough_date(id_date_raw: String) -> (String, NaiveDate) {
     let mut id_vec = id_date_raw.split_whitespace().map(String::from).collect::<Vec<String>>();
     let date_vec = id_vec.split_off(2);
-
     let id_string = id_vec.iter().fold(String::from(""), |acc, x| acc + " " + &x);
     let date_string = date_vec.iter().fold(String::from(""), |acc, x| acc + " " + &x);
-    
+    let date_naive = parse_rough_date(&date_string).unwrap();
+   
+    (id_string, date_naive)
+}
 
+/// A rough paper gathers the information we can get from the summary page. Notice that the link and the date are not correct.
+/// The link is a link to the details page
+/// the date is just a date without any time.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct RoughPaper {
+    pub title: String,
+    pub details_link: String,
+    pub source: Source,
+    pub rough_published: NaiveDate,
+    pub authors: Vec<String>,
+}
 
-    //let id = whitespace.take(2).collect::<String>();
-    //let datestring = whitespace.skip(2).collect::<String>();
+/// takes one div and returns the parsed rough paper
+fn parse_single_div(div: Node) -> RoughPaper {
+    let id_and_date_raw = div.find(Name("u")).nth(0).unwrap().text();
+    let link_raw = div.find(Name("a")).nth(0).unwrap().attr("href").unwrap();
+    let title_raw = div.find(Name("h4")).nth(0).unwrap().text();
+
+    let authors_raw = div.children().nth(1).unwrap().text();
     
-    let date = parse_rough_date(&date_string);
-    println!("{}, \"{:?}\"", date_string, date);
-    
-    //Date::parse_from_str(, "")
-    println!("{:?}", id_string);
-    return ("".to_string(), "".to_string());
+    let (id, date) = extract_id_rough_date(id_and_date_raw);
+    let link = BASE_URL.to_owned() + link_raw.trim();
+    let title = title_raw.trim();
+
+    let authors = extract_authors(authors_raw);
+
+    //we need to get the full abstract and full time from the details page
+    RoughPaper { title: title.to_string(), details_link: link.to_string(), source: Source::ECCC, rough_published: date, authors: authors } 
 }
 
 /// The full abstract is in the details so we better filter before we look into the details of a million papers. Hence this function needs a filter time
@@ -110,30 +132,12 @@ pub fn parse_eccc(utc: DateTime<Utc>) -> Result<Vec<Paper>> {
     
     let parsedoc = Document::from(res_string.as_str());
     //println!("res_string: {}", res_string);
-    let mut divs = parsedoc.find(And(Name("div"),Attr("id", "box"))).take(1);
+    let divs = parsedoc.find(And(Name("div"),Attr("id", "box"))).take(1);
     
     //println!("{:?}", divs.nth(0).unwrap());
-
-    for div in divs {
-        let id_and_date_raw = div.find(Name("u")).nth(0).unwrap().text();
-        let link_raw = div.find(Name("a")).nth(0).unwrap().attr("href").unwrap();
-        let title_raw = div.find(Name("h4")).nth(0).unwrap().text();
-//        let abs_raw = div.find(And(Name("div"), Attr("style", "text-align:justify;"))).nth(0).unwrap().text();
-        let authors_raw = div.children().nth(1).unwrap().text();
-
-        let (id, date) = extract_id_rough_date(id_and_date_raw);
-        let link = link_raw.trim();
-        let title = title_raw.trim();
-//        let abs = abs_raw.trim();
-        let authors = extract_authors(authors_raw);
-
-
-        //we need to get the full abstract from the details page
-        //println!("authors: {}", authors);
-        println!("Parsed: idd{:?}, link{:?}, title{:?}, authors{:?}", date, link, title, authors);
-    } 
     
-    
+    let roughpapers = divs.map(parse_single_div).collect::<Vec<RoughPaper>>();    
+    println!("{:?}", roughpapers);
     
     return Ok(vec![]);
     
